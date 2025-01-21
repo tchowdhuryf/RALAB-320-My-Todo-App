86e95fcac0a75440c13d2fdf12d1eb577b9617e9
tchowdhuryf <tasnim.chowdhury837@gmail.com>
2025-01-21 11:38:42 -0500

    added css for list and list item. added checkbox and item name to show on the list menu

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "./Item.module.css";
 
 const Item = ({ item }) => {
-  return <div>{item.name}</div>;
+  return (<li className={styles.item}>
+    <div className={styles["item-group"]}>
+    <input 
+    type="checkbox" 
+    />
+    {item.name}
+    </div>
+    </li> )
 };
 
 export default Item;
